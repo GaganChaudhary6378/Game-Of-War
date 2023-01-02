d82126fe31ad49ef747f38ad11978d4774c37764
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-01-03 02:02:23 +0530

    finally done

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let deckId;
+let computerScore=0;
+let myScore=0;
+let computerEl=document.getElementById("comp-score");
+let myScoreEl=document.getElementById("human-score");
 let imageEl1=document.getElementById("img-el1");
 let imageEl2=document.getElementById("img-el2");
 let warEl=document.getElementById("war");
+let drawBtn=document.getElementById("draw-el");
+let remainingEl=document.getElementById("cont-el");
 function handleClick(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
+        remainingEl.textContent=`Remaining Cards: ${data.remaining}`
         deckId=data.deck_id;
     })
 }
@@ -17,6 +24,7 @@ function getCards(){
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
+        remainingEl.textContent=`Remaining Cards: ${data.remaining}`
         imageEl1.innerHTML=`
         <img src=${data.cards[0].image} />
         `
@@ -25,11 +33,22 @@ function getCards(){
         `
         const winner=determineCardWinner(data.cards[0],data.cards[1]);
         warEl.textContent=winner;
+
+        if(data.remaining==0){
+            drawBtn.disabled=true;  
+            if(computerScore>myScore){
+                warEl.textContent="The computer won the game!"
+            }else if(myScore>computerScore){
+                warEl.textContent="You won the game!"
+            }else{
+                warEl.textContent="It's a tie game!"
+            }
+        }
     })
 
 }
 
-document.getElementById("draw-el").addEventListener('click',getCards)
+drawBtn.addEventListener('click',getCards)
 
 function determineCardWinner(card1,card2){
     const valueOptions=["2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"]
@@ -37,9 +56,13 @@ function determineCardWinner(card1,card2){
     const card2ValueIndex=valueOptions.indexOf(card2.value)
 
     if(card1ValueIndex>card2ValueIndex){
-        return "Card 1 wins!"
+        computerScore++;
+        computerEl.textContent=`Computer: ${computerScore}`
+        return "Computer wins!"
     }else if(card2ValueIndex>card1ValueIndex){
-        return "Card 2 wins!"
+        myScore++
+        myScoreEl.textContent=`Me: ${myScore}`
+        return "You win!"
     }else{
         return "War!"
     }

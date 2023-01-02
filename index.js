81e79b48e575b33a49fbbd010dd7c8ac82cf9020
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-01-03 01:05:30 +0530

    basic part done

---
+++ index.js
@@ -0,0 +1,46 @@
+let deckId;
+let imageEl1=document.getElementById("img-el1");
+let imageEl2=document.getElementById("img-el2");
+let warEl=document.getElementById("war");
+function handleClick(){
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => {
+        deckId=data.deck_id;
+    })
+}
+
+document.getElementById("new-deck").addEventListener('click',handleClick)
+
+
+function getCards(){
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+        imageEl1.innerHTML=`
+        <img src=${data.cards[0].image} />
+        `
+        imageEl2.innerHTML=`
+        <img src=${data.cards[1].image} />
+        `
+        const winner=determineCardWinner(data.cards[0],data.cards[1]);
+        warEl.textContent=winner;
+    })
+
+}
+
+document.getElementById("draw-el").addEventListener('click',getCards)
+
+function determineCardWinner(card1,card2){
+    const valueOptions=["2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"]
+    const card1ValueIndex=valueOptions.indexOf(card1.value)
+    const card2ValueIndex=valueOptions.indexOf(card2.value)
+
+    if(card1ValueIndex>card2ValueIndex){
+        return "Card 1 wins!"
+    }else if(card2ValueIndex>card1ValueIndex){
+        return "Card 2 wins!"
+    }else{
+        return "War!"
+    }
+} 
